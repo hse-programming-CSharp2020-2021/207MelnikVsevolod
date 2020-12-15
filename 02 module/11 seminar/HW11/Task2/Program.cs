fc227a75b80103a121e64bbe049b4a17a86bc5dc
Vsevolod Melnik <vsevolodkm@gmail.com>
2020-12-15 14:34:08 +0300

    Home work 11 (12?) done

---
+++ 02 module/11 seminar/HW11/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] lines;
+            if ((lines = StringSplit.ValidatedSplit(str, ';')) == null)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                for (int i = 0; i < lines.Length; ++i)
+                    Console.WriteLine(StringSplit.Abbrevation(lines[i]));
+            }
+        }
+    }
+}
